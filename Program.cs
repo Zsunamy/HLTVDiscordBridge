ad93a68c7fb13e1a1d1da0a8255c7695181667b8
Drachenfalke12 <marco.pa@gmx.de>
2021-02-08 19:49:28 +0100

    Admin push

--- Program.cs
+++ Program.cs
@@ -48,6 +48,10 @@ namespace HLTVDiscordBridge
             _client.Log += Log;
             _client.ReactionAdded += ReactionAdd;
             _client.JoinedGuild += GuildJoined;
+            foreach (SocketGuild guild in _client.Guilds) 
+            {
+                await guild.LeaveAsync();
+            }
 
             await RegisterCommandsAsync();
 
