4f08a0f3f49e9d3afffdf67358fbf266dba90ab9
Nils Jacobs <revilum.hi@gmail.com>
2022-11-22 16:52:34 +0100

    fixed bullshit

--- Program.cs
+++ Program.cs
@@ -181,7 +181,7 @@ namespace HLTVDiscordBridge
                         await Task.Delay(_botconfig.CheckResultsTimeInterval / 4); watch.Restart();
                         await HltvNews.SendNewNews();
                         WriteLog($"{DateTime.Now.ToLongTimeString()} HLTV\t\t fetched news ({watch.ElapsedMilliseconds}ms)"); watch.Restart();
-                        CacheCleaner.Cleaner(_client);
+                        // CacheCleaner.Cleaner(_client);
                         await Task.Delay(_botconfig.CheckResultsTimeInterval / 4);
                     } catch (Exception ex)
                     {
